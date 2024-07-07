a02039b649da157e4b62c0fcdd1b06c95908ef4c
Chawit Chaijirawiwat <chawitcgithub@gmail.com>
2024-07-07 16:39:18 +0900

    edit test_app.py for test formatting

--- test_app.py
+++ test_app.py
@@ -1,20 +1,14 @@
 import pytest
 from app.webapp import app
-
-
 @pytest.fixture
 def client():
     with app.test_client() as client:
         with app.app_context():
             yield client
-
-
 def test_home(client):
     response = client.get("/")
     assert response.status_code == 200
     assert b"App Home: From Azure Pipelines (Continuous Delivery)" in response.data
-
-
 def test_predict(client):
     response = client.post("/predict", json={"input": "test string"})
     assert response.status_code == 200
