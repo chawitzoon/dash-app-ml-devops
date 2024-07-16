cf378239d4804042610a73837fb679235b969017
Chawit Chaijirawiwat <chawitcgithub@gmail.com>
2024-07-16 22:54:12 +0900

    add post request function and html templates

--- app/views.py
+++ app/views.py
@@ -1,34 +1,26 @@
-from flask import Flask, request, jsonify
+from flask import request, render_template, redirect, url_for
 from flask.logging import create_logger
+import requests
 import logging
 from . import app
 
 LOG = create_logger(app)
 LOG.setLevel(logging.INFO)
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-
-@app.route("/")
-def home():
-    html = (
-        "<h3>App Home: From Azure Pipelines (Continuous Delivery) updated version git diff</h3>"
-    )
-    return html.format(format)
-
-
-@app.route("/predict", methods=["POST"])
+@app.route('/predict', methods=['POST'])
 def predict():
+    prices = request.form.get('prices')
+    prices_list = [float(x) for x in prices.split(',')]
+
+    payload = {"prices": prices_list}
     try:
-        data = request.json
-        input_str = data.get('input', '')
-        # For testing, return the input string
-        prediction = f'Processed input: {input_str}'
-        return jsonify({"prediction": prediction})
-    except KeyError as e:
-        LOG.error("KeyError: %s", e)
-        return jsonify({"error": "KeyError occurred"}), 400
-    except TypeError as e:
-        LOG.error("TypeError: %s", e)
-        return jsonify({"error": "TypeError occurred"}), 400
-    except Exception as e:
-        LOG.error("Unexpected error: %s", e)
-        return jsonify({"error": "An unexpected error occurred"}), 500
+        response = requests.post("http://127.0.0.1:8080/predict_next_price", json=payload)
+        response.raise_for_status()
+        result = response.json().get("result")
+    except requests.RequestException as e:
+        return f"An error occurred: {e}", 500
+
+    return render_template('result.html', result=result)
