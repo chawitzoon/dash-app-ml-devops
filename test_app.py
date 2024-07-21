18ef57fffa0eab3a4f774b6ea540ffd5048df329
Chawit Chaijirawiwat <chawitcgithub@gmail.com>
2024-07-21 15:19:30 +0900

    update test_app.py to match with updated flask app

--- test_app.py
+++ test_app.py
@@ -1,6 +1,7 @@
 import pytest
 from app.webapp import app
-
+import requests
+from unittest.mock import patch
 
 @pytest.fixture
 def client():
@@ -8,14 +9,29 @@ def client():
         with app.app_context():
             yield client
 
-
-def test_home(client):
+def test_index(client):
     response = client.get("/")
     assert response.status_code == 200
-    assert b"App Home: From Azure Pipelines (Continuous Delivery)" in response.data
+    assert b"Predict Next Price" in response.data
 
+@patch('requests.post')
+def test_predict(mock_post, client):
+    mock_post.return_value.status_code = 200
+    mock_post.return_value.json.return_value = {"result": 130.0}
 
-def test_predict(client):
-    response = client.post("/predict", json={"input": "test string"})
+    response = client.post("/predict", json={"prices": [120.0, 125.0, 123.0, 127.0, 126.0, 128.0, 130.0]})
     assert response.status_code == 200
-    assert response.get_json() == {"prediction": "Processed input: test string"}
+    assert response.get_json() == {"result": 130.0}
+
+    mock_post.assert_called_once_with("http://127.0.0.1:8080/predict_next_price", json={"prices": [120.0, 125.0, 123.0, 127.0, 126.0, 128.0, 130.0]})
+
+def test_stock_data(client):
+    response = client.get("/stock_data")
+    assert response.status_code == 200
+
+    data = response.get_json()
+    assert "timestamp" in data
+    assert "open" in data
+    assert "high" in data
+    assert "low" in data
+    assert "close" in data
